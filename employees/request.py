7a982f3f0a9ff3993bd4627cdd3e5377004fb822
Erin <etruman2@gmail.com>
2021-10-11 14:42:04 -0500

    first commit

---
+++ employees/request.py
@@ -0,0 +1,29 @@
+EMPLOYEES = [
+    {
+      "id": 1,
+      "name": "Jessica Younker",
+      "email": "jessica@younker.com",
+    },
+    {
+      "id": 3,
+      "name": "Zoe LeBlanc",
+      "email": "zoe@leblanc.com",
+    },
+    {
+      "name": "Hannah Hall",
+      "email": "hannah@hall.com",
+      "id": 5,
+    },
+    {
+      "name": "Jenna Solis",
+      "email": "jenna@solis.com",
+      "id": 14,
+    },
+    {
+      "id": 15,
+      "name": "Ryan Tanay",
+      "email": "ryan@tanay.com",
+    }
+  ]
+def get_all_employees():
+    return EMPLOYEES
---
+++ locations/request.py
@@ -0,0 +1,13 @@
+LOCATIONS = [
+    {
+      "id": 1,
+      "name": "Nashville North"
+    },
+    {
+      "id": 2,
+      "name": "Nashville South"
+    }
+  ]
+
+def get_all_locations():
+    return LOCATIONS
